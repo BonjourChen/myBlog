d6533aae31f77c5fe681f744681e97c7bd36b92a
bonjourchen <346517772@qq.com>
2016-03-21 17:22:19 +0800

    processing...

--- app/models.py
+++ app/models.py
@@ -29,4 +29,24 @@ class Post(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	body = db.Column(db.Text)
 	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+	body_html = db.Column(db.Text)
+
+	@staticmethod
+	def on_changed_body(target, value, oldvalue, initiator):
+		allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i',
+						'li', 'ol','pre', 'strong', 'ul', 'h1', 'h2', 'h3', 'p']
+		target.body_html = bleach.linkify(bleach.clean(
+			markdown(value, output_format='html'),
+			tags=allowed_tags, strip=True))
+
+	@staticmethod
+	def on_changed_summary(target, value, oldvalue, initiator):
+		allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i',
+						'li', 'ol','pre', 'strong', 'ul', 'h1', 'h2', 'h3', 'p']
+		target.body_html = bleach.linkify(bleach.clean(
+			markdown(value, output_format='html'),
+			tags=allowed_tags, strip=True))
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
+db.event.listen(Post.body, 'set', Post.on_changed_summary)
 	# category_id = db.Column(db.Integer,db.ForeignKey('categorys.id'))
